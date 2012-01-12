c4e5802005c1fa090e3bb58c1e5c4ffccf102e16
urara036 <oura036@gmail.com>
2012-01-12 16:44:11 +0900

    first commit

---
+++ Shakyou/test/HelloJUnitTest.java
@@ -0,0 +1,11 @@
+//package jp.co.shoeisha;
+
+import org.junit.Test;
+
+public class HelloJUnitTest {
+
+    @Test
+    public void HelloTest() {
+        System.out.println("Hello JUnit.");
+    }
+}
