78cf063f68706965672f59c055818661caac1dcc
urara036 <oura036@gmail.com>
2012-02-02 19:26:17 +0900

    2/2 19:26

--- Shakyou/src/com/shakyou/Main.java
+++ Shakyou/src/com/shakyou/Main.java
@@ -4,15 +4,12 @@ import java.util.Date;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DateShakyou date = new DateShakyou();
+		//DateShakyou date = new DateShakyou(1L);
 		Date dateorg = new java.util.Date();
-		//System.out.println(date.toString() + " " + dateorg.toString() + " " + System.currentTimeMillis());
-		System.out.println(date.toString());
+		DateShakyou date = new DateShakyou();
+		System.out.println(date.toString() + " " + dateorg.toString() + " " + System.currentTimeMillis());
+		//System.out.println(date.toString());
 	}
-
 }
